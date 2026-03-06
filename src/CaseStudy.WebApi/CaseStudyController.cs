6058555b50b48c43dcc6b6750a1e173c8d111795
Sarka Mejzlikova <mejzlikova.s@gmail.com>
2026-03-06 14:02:02 +0100

    Prepare_solution

---
+++ src/CaseStudy.WebApi/CaseStudyController.cs
@@ -0,0 +1,15 @@
+namespace CaseStudy.WebApi;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CaseStudyController : ControllerBase
+{
+    
+}
